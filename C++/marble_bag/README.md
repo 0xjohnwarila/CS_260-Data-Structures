6dec27d5bfa97434f54543a5c2dc87036a32307b
JohnNWarila <johnwarila@gmail.com>
2019-04-06 19:21:14 -0700

    Added README.md for marble_bag. Refactored tests in marble_bag.

---
+++ C++/marble_bag/README.md
@@ -0,0 +1,5 @@
+To compile, run make in terminal.
+
+To run, run ./main in terminal.
+
+Main contains tests for the bag and marble modules.
--- C++/marble_bag/main.cpp
+++ C++/marble_bag/main.cpp
@@ -3,40 +3,33 @@
 #include "bag.hpp"
 #include "marble.hpp"
 
-bool strEqual(std::string input1, std::string input2) {
-  if (input1 == input2)
-    return true;
-  else
-    return false;
-}
-
 bool testMarbleColor(std::string testColor) {
   Marble testMarble = Marble(testColor, "");
-  return (strEqual(testMarble.color(), testColor));
+  return testMarble.color() == testColor;
 }
 
 bool testMarblePattern(std::string testPattern) {
   Marble testMarble = Marble("", testPattern);
-  return (strEqual(testMarble.pattern(), testPattern));
+  return testMarble.pattern() == testPattern;
 }
 
 bool testMarbleSetColor(std::string testColor) {
   Marble testMarble = Marble();
   testMarble.setColor(testColor);
-  return (strEqual(testMarble.color(), testColor));
+  return testMarble.color() == testColor;
 }
 
 bool testMarbleSetPattern(std::string testPattern) {
   Marble testMarble = Marble();
   testMarble.setPattern(testPattern);
-  return (strEqual(testMarble.pattern(), testPattern));
+  return testMarble.pattern() == testPattern;
 }
 
 bool testBagAddMarble(Marble testMarble) {
   Bag testBag = Bag();
   testBag.addMarble(testMarble);
   Marble returnedMarble = testBag.getMarble();
-  return (strEqual(testMarble.color(), returnedMarble.color()) && strEqual(testMarble.pattern(), returnedMarble.pattern()));
+  return (testMarble.color() == returnedMarble.color()) && (testMarble.pattern() == returnedMarble.pattern());
 }
 
 bool testBagMarbleCount(int marbleNum) {
@@ -45,10 +38,7 @@ bool testBagMarbleCount(int marbleNum) {
     Marble newMarble = Marble();
     testBag.addMarble(newMarble);
   }
-  if (testBag.marbleCount() == marbleNum)
-    return true;
-  else
-    return false;
+  return testBag.marbleCount() == marbleNum;
 }
 
 int main() {
