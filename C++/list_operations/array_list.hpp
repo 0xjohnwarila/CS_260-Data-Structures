9ed7d0387979d495e3be0d8efd86356de1273748
JohnNWarila <johnwarila@gmail.com>
2019-04-19 09:23:56 -0700

    Added array list

--- C++/list_operations/array_list.hpp
+++ C++/list_operations/array_list.hpp
@@ -1,29 +1,55 @@
 #pragma once
 
 #include <iostream>
+#include <new>
 
 template <class T>
 class ArrayList {
  private:
-  T *array = nullptr;
+  T *arrList;
+  int arrLength;
   bool empty(void);
 
  public:
   ArrayList(){};
   void add(T inVal, int inPos);
+  void fill(T inArr[], int length);
   T remove(int pos);
   T get(int pos);
 };
 
 template <class T>
 void ArrayList<T>::add(T inVal, int inPos) {
-  std::cout << "Adding " << inVal << " to position " << inPos << std::endl;
-
-  if (empty()) {
-    // if empty: allocate memory to the size of inPos & set array[inPos] to inVal
-    array = new T[inPos];
-    array[inPos] = inVal;
+  if (inPos > arrLength) {
+    // This is probably terribly inefficient, but its a start
+    T *tempArr = new (std::nothrow) T[inPos + 1];
+    if (tempArr == nullptr) {
+      std::cout << "Mem allocation error" << std::endl;
+      return;
+    }
+    for (int i = 0; i < arrLength + 1; i++) {
+      tempArr[i] = arrList[i];
+    }
+    tempArr[inPos] = inVal;
+    fill(tempArr, inPos + 1);
   } else {
-    // if !empty:
+    arrList[inPos] = inVal;
   }
+}
+
+template <class T>
+void ArrayList<T>::fill(T inArr[], int length) {
+  arrList = new (std::nothrow) T[length];
+  if (arrList == nullptr) {
+    std::cout << "Mem allocation error" << std::endl;
+    return;
+  }
+  for (int i = 0; i < length; i++) {
+    arrList[i] = inArr[i];
+  }
+}
+
+template <class T>
+T ArrayList<T>::get(int pos) {
+  return arrList[pos];
 }
--- C++/list_operations/main.cpp
+++ C++/list_operations/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "array_list.hpp"
 #include "linked_list.hpp"
 #include "test_array_list.hpp"
 
@@ -15,5 +16,14 @@ int main() {
     std::cout << intList.get(i) << std::endl;
   }
 
+  std::cout << "Array Testing" << std::endl;
+  ArrayList<int> intArr;
+  int testArr[4] = {1, 2, 3, 4};
+  intArr.fill(testArr, 4);
+  std::cout << "0 = " << intArr.get(0) << std::endl;
+  intArr.add(testArr[1], 0);
+  std::cout << "0 = " << intArr.get(0) << std::endl;
+  intArr.add(99, 8);
+  std::cout << "8 = " << intArr.get(8) << std::endl;
   return EXIT_SUCCESS;
 }
