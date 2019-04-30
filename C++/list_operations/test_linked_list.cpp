1eef984679740d553310725ccaa429378ca2a542
JohnNWarila <johnwarila@gmail.com>
2019-04-30 15:57:21 -0700

    Finished testing of linked lists

--- C++/list_operations/test_linked_list.cpp
+++ C++/list_operations/test_linked_list.cpp
@@ -7,12 +7,12 @@
 using std::cout;
 using std::endl;
 
-bool insertLinkedListTest(void) {
+bool insertGetLinkedListTest(void) {
   cout << "*** Insert Test ***" << endl
        << endl;
   std::array<int, 5> testData = {15, 2, 32, 45, 51};
-  std::array<int, 6> expectedData = {51, 45, 42, 32, 2, 15};
-  std::array<int, 6> returnData;
+  std::array<int, 7> expectedData = {51, 45, 42, 32, 2, 15, 88};
+  std::array<int, 7> returnData;
   LinkedList<int> newIntList;
 
   for (int i = 0; i < 5; i++) {
@@ -20,8 +20,9 @@ bool insertLinkedListTest(void) {
   }
 
   newIntList.insert(42, 2);
+  newIntList.insert(88, 6);
 
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 7; i++) {
     returnData[i] = newIntList.get(i);
   }
 
@@ -36,46 +37,35 @@ bool insertLinkedListTest(void) {
   }
 }
 
-bool appendLinkedListTest(void) {
-  cout << "*** Append Test ***" << endl
-       << endl;
-  cout << "Append Test Failed!" << endl
-       << endl;
-  return false;
-}
-
-bool getLinkedListTest(void) {
-  cout << "*** Get Test ***" << endl
-       << endl;
-  cout << "Get Test Failed!" << endl
-       << endl;
-  return false;
-}
-
 bool removeLinkedListTest(void) {
   cout << "*** Remove Test ***" << endl
        << endl;
-  cout << "Remove Test Failed!" << endl
-       << endl;
-  return false;
-}
+  std::array<int, 5> testData = {15, 2, 32, 45, 51};
+  std::array<int, 4> expectedData = {15, 2, 32, 51};
+  std::array<int, 4> returnData;
+  LinkedList<int> newIntList;
 
-bool iteratorLinkedListTest(void) {
-  cout << "*** Iterator Test ***" << endl
-       << endl;
-  cout << "Iterator Test Failed!" << endl
-       << endl;
-  return false;
-}
+  for (int i = 5 - 1; i >= 0; i--) {
+    newIntList.insert(testData[i]);
+  }
 
-bool edgeCaseLinkedListTest(void) {
-  cout << "*** Edge Case Test ***" << endl
-       << endl;
-  cout << "Edge Case Test Failed!" << endl
-       << endl;
-  return false;
+  newIntList.remove(3);
+
+  for (int i = 0; i < 4; i++) {
+    returnData[i] = newIntList.get(i);
+  }
+
+  if (returnData == expectedData) {
+    cout << "Remove Test Passed!" << endl
+         << endl;
+    return true;
+  } else {
+    cout << "Remove Test Failed!" << endl
+         << endl;
+    return false;
+  }
 }
 
 bool linked_list_TestSuite(void) {
-  return (insertLinkedListTest() && appendLinkedListTest() && getLinkedListTest() && removeLinkedListTest() && iteratorLinkedListTest() && edgeCaseLinkedListTest());
+  return (insertGetLinkedListTest() && removeLinkedListTest());
 }
