da443247689ee3687b1d202d8f3a5b76d7d24ca7
JohnNWarila <johnwarila@gmail.com>
2019-04-29 21:58:19 -0700

    Added testing methods for linked list (failing)

--- C++/list_operations/test_linked_list.cpp
+++ C++/list_operations/test_linked_list.cpp
@@ -2,6 +2,57 @@
 
 #include "test_linked_list.hpp"
 
-bool linked_list_TestSuite(void) {
+using std::cout;
+using std::endl;
+
+bool insertLinkedListTest(void) {
+  cout << "*** Insert Test ***" << endl
+       << endl;
+  cout << "Insert Test Failed!" << endl
+       << endl;
   return false;
+}
+
+bool appendLinkedListTest(void) {
+  cout << "*** Append Test ***" << endl
+       << endl;
+  cout << "Append Test Failed!" << endl
+       << endl;
+  return false;
+}
+
+bool getLinkedListTest(void) {
+  cout << "*** Get Test ***" << endl
+       << endl;
+  cout << "Get Test Failed!" << endl
+       << endl;
+  return false;
+}
+
+bool removeLinkedListTest(void) {
+  cout << "*** Remove Test ***" << endl
+       << endl;
+  cout << "Remove Test Failed!" << endl
+       << endl;
+  return false;
+}
+
+bool iteratorLinkedListTest(void) {
+  cout << "*** Iterator Test ***" << endl
+       << endl;
+  cout << "Iterator Test Failed!" << endl
+       << endl;
+  return false;
+}
+
+bool edgeCaseLinkedListTest(void) {
+  cout << "*** Edge Case Test ***" << endl
+       << endl;
+  cout << "Edge Case Test Failed!" << endl
+       << endl;
+  return false;
+}
+
+bool linked_list_TestSuite(void) {
+  return (insertLinkedListTest() && appendLinkedListTest() && getLinkedListTest() && removeLinkedListTest() && iteratorLinkedListTest() && edgeCaseLinkedListTest());
 }
