eb9f445f0978b3bf1392fc118037efbf71168868
JohnNWarila <johnwarila@gmail.com>
2019-06-01 15:53:31 -0700

    Added documentation to the advancedHashTable

--- C++/hashtable/advancedhashtable.hpp
+++ C++/hashtable/advancedhashtable.hpp
@@ -10,6 +10,7 @@
 #include "linked_list.hpp"
 #include "person.hpp"
 
+// Quick and dirty storage method for the index and depth an object is stored at
 struct depthIndex {
   size_t index;
   size_t depth;
@@ -49,6 +50,8 @@ class AdvancedHashTable {
 // CONSTRUCTOR
 //
 
+// Constructor, put a size in
+// O(n)
 template <class T>
 AdvancedHashTable<T>::AdvancedHashTable(size_t size) {
   createTable(size);
@@ -61,19 +64,23 @@ AdvancedHashTable<T>::AdvancedHashTable(size_t size) {
 // METHODS
 //
 
-// Returns the number of objects in table O(1)
+// Returns the number of objects in table
+// O(1)
 template <class T>
 int AdvancedHashTable<T>::objectCount(void) const {
   return objectCount_;
 }
 
-// Returns size of table O(1)
+// Returns size of table
+// O(1)
 template <class T>
 size_t AdvancedHashTable<T>::size(void) const {
   return size_;
 }
 
 // ADD DOCUMENTATION HERE JOHN. BEFORE YOU TURN IT IN
+// Takes T& inObject, generates an index from a hash of the key, then inserts it into the list
+// O(n)
 template <class T>
 void AdvancedHashTable<T>::add(const T& inObject) {
   const std::string key = inObject.key();
@@ -81,6 +88,8 @@ void AdvancedHashTable<T>::add(const T& inObject) {
   insert(index, inObject);
 }
 
+// Takes a std::string key, then finds the index and depth of the object with the key and removes it
+// O(n) <- because of find(key)
 template <class T>
 void AdvancedHashTable<T>::remove(std::string key) {
   // Find the index of the object to be removed
@@ -90,6 +99,9 @@ void AdvancedHashTable<T>::remove(std::string key) {
     setNull(dIndex);
 }
 
+// Takes a std::string key, finds the index and depth of the object with the key and returns it.
+// If there is no matching object, it returns a nullobject.
+// O(n) <- because of find(key);
 template <class T>
 T AdvancedHashTable<T>::get(std::string key) {
   // Find the index to get
@@ -107,6 +119,8 @@ T AdvancedHashTable<T>::get(std::string key) {
 // METHODS
 //
 
+// Fills the table vector with vectors of size 1 with a nullobject in them
+// O(n)
 template <class T>
 void AdvancedHashTable<T>::createTable(size_t size) {
   for (size_t i = 0; i < size; i++) {
@@ -115,6 +129,12 @@ void AdvancedHashTable<T>::createTable(size_t size) {
   }
 }
 
+// Finds the object with the key provided.
+// First it gets the index from hashing the key
+// Then it walks down the index until it hits the object
+// When it finds the object, it will return a struct of the index and depth
+// If it does not find the object, it will return a struct with size_ as the index
+// O(n)
 template <class T>
 depthIndex AdvancedHashTable<T>::find(const std::string key) const {
   size_t hashVal = hash(key, size_);
@@ -128,12 +148,20 @@ depthIndex AdvancedHashTable<T>::find(const std::string key) const {
   return {size_, 0};
 }
 
+// Hashes the key with the hash function fnv1a.
+// O(n)
 template <class T>
 size_t AdvancedHashTable<T>::hash(const std::string key, size_t size) const {
   uint32_t hashReturn = FNV::fnv1a(key);
   return hashReturn % size;
 }
 
+// Takes an index, and an T& inObject and places it at the appropriate depth at the index
+// It will go to the index, and walk down it until it hits a nullObject or a copy
+// If it finds nullObject, it will insert there and return
+// If it finds a copy it will do nothing and return
+// If it finds neither it will push the object to the end of the vector
+// O(n)
 template <class T>
 void AdvancedHashTable<T>::insert(const size_t index, const T& inObject) {
   std::vector<T> currentList = table.at(index);
@@ -154,6 +182,8 @@ void AdvancedHashTable<T>::insert(const size_t index, const T& inObject) {
   objectCount_++;
 }
 
+// Takes a depthIndex and sets the object there to null
+// O(1)
 template <class T>
 void AdvancedHashTable<T>::setNull(depthIndex dIndex) {
   table.at(dIndex.index).at(dIndex.depth) = T(true);
